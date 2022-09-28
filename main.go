9fb878e1c22deec353013530f1defa5bb199ab4d
Sean O'Neill <seansaysmeh@gmail.com>
2022-09-28 12:00:27 +0100

    Add file handler and unit tests

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	f := NewFileHandler("./static")
+	http.HandleFunc("/", f.handle)
 
-	log.Print("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
